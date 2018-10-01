6df1d5a2afa49cfe70e0504d1551773cc696998b
15005688 <sullivan.dairou@etudiant.univ-rennes1.fr>
2018-10-01 16:45:50 +0200

    ajouter methode exporter CSV

--- Groupe1/src/main/java/PDL_2018_2019/Groupe1/App.java
+++ Groupe1/src/main/java/PDL_2018_2019/Groupe1/App.java
@@ -13,10 +13,13 @@ package PDL_2018_2019.Groupe1;
 }*/
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.regex.Matcher;
@@ -32,11 +35,34 @@ import javax.swing.text.BadLocationException;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FileNotFoundException
     {
-       // Extraction();
-    	ConvertirAdresseWikiCode("https://fr.wikipedia.org/wiki/Ordre_du_Temple");
+       Extraction();
+    	//ConvertirAdresseWikiCode("https://en.wikipedia.org/wiki/Comparison_of_Canon_EOS_digital_cameras");
+    	//exporterCSV("e");
     	
+    }
+    
+    
+    /**
+     * ouverture du fichier adresse.txt
+     * lire toutes les lignes (url)
+     */
+    public static void Extraction() {
+    	try{
+    		InputStream flux=new FileInputStream("adresse.txt"); 
+    		InputStreamReader lecture=new InputStreamReader(flux);
+    		BufferedReader buff=new BufferedReader(lecture);
+    		String ligne;
+    		while ((ligne=buff.readLine())!=null){
+    			System.out.println(ligne);
+    			System.out.println(verificationUrl(ligne));
+    		}
+    		buff.close(); 
+    		}		
+    		catch (Exception e){
+    		System.out.println(e.toString());
+    		}
     }
     
     /* Convertir Url Entrée en URL WikiCode
@@ -60,49 +86,18 @@ public class App
            System.out.println("NO MATCH");
         }
     }
-   
-    /**
-     * ouverture du fichier adresse.txt
-     * lire toutes les lignes (url)
-     */
-    public static void Extraction() {
-    	try{
-    		InputStream flux=new FileInputStream("adresse.txt"); 
-    		InputStreamReader lecture=new InputStreamReader(flux);
-    		BufferedReader buff=new BufferedReader(lecture);
-    		String ligne;
-    		while ((ligne=buff.readLine())!=null){
-    			System.out.println(ligne);
-    			System.out.println(verificationUrl(ligne));
-    		}
-    		buff.close(); 
-    		}		
-    		catch (Exception e){
-    		System.out.println(e.toString());
-    		}
-    }
+
     /*
      * Param : Url
      * Précondiction = True if adresse Wiki faux sinon
      *
      */
     public static boolean verificationUrl(String url) throws IOException, BadLocationException {
-    	String URLValide="https://fr.wikipedia.org";
+    	String URLValide=".wikipedia.org";
     	if (url.contains(URLValide)) {
-    		getTextFile(URLValide);
+    		getTextFile(url);
     	}
     	return url.contains(URLValide);
-    }
-    
-    /*
-     * récupère le html
-     */
-    public static void BaliseTableau(String txtHtml) {
-    	Pattern p = Pattern.compile(".*<tr(.*)</tr>.*");
-    	Matcher match = p.matcher(txtHtml);
-    	if(match.matches())
-            System.out.println(match.group(1));
-
     }
      
     /*
@@ -140,9 +135,12 @@ public class App
             
                 //sb.append("\n");
             }
-           System.out.println(sb.toString());
-           // BaliseTableau(sb.toString());
-            ExtraireWikiCode(sb.toString());
+            System.out.println("bien");;
+            System.out.println(sb.toString());
+           //BaliseTableau(sb.toString());
+           // ExtraireWikiCode(sb.toString());
+            
+            exporterCSV(sb.toString());
             return sb.toString();
             
         } catch (IOException ex) {
@@ -157,6 +155,35 @@ public class App
 
             }
         }
+    }
+    /*
+     * récupère le html
+     */
+    public static void BaliseTableau(String txtHtml) {
+    	Pattern p = Pattern.compile(".*<tr(.*)</tr>.*");
+    	Matcher match = p.matcher(txtHtml);
+    	if(match.matches())
+            System.out.println(match.group(1));
+
+    }
+    
+    /*
+     * Exporter vers CSV
+     */
+    
+    public static void exporterCSV(String Text) throws FileNotFoundException {
+    	
+    	PrintWriter pw = new PrintWriter(new File("test_enregistrer_csv.csv"));
+          StringBuilder sb = new StringBuilder();
+          //System.out.println(Text);
+          sb.append(Text);
+
+
+          pw.write(Text);
+          pw.close();
+          
+          System.out.println("done!");
+    	
     }
 
 } 

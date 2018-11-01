3d17af103a94c466f921aed232e43ce0f6f810d5
Margaux <Margaux@DESKTOP-V88FTER.home>
2018-11-01 23:50:05 +0100

    Fonction isValidUrl()

--- src/main/java/pdl_2018/groupeSMKS1/Url.java
+++ src/main/java/pdl_2018/groupeSMKS1/Url.java
@@ -1,5 +1,33 @@
 package src.main.java.pdl_2018.groupeSMKS1;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.IOException;
 
-public class Url implements IUrl{
+public class Url implements IUrl{ 
+
+/**
+ * 
+ * @param url
+ * @return true si l'URL est valide, false sinon
+ * 
+ */
+public boolean isValidURL(String url) {  
+URL u = null; 
+try { 
+    u = new URL(url); 
+} catch (MalformedURLException e) { 
+    return false; 
+} 
+try { 
+    URLConnection c = u.openConnection(); 
+    c.connect(); 
+} catch (IOException e) { 
+    return false; 
+} 
+return true; 
+} 
+
+
 
 }

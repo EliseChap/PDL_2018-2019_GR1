8a2237379c1c2dd8b2f85a72b2ec68559a2e4f37
Margaux <Margaux@DESKTOP-V88FTER.home>
2018-11-02 22:38:19 +0100

    isWikipediaURL version beta

--- src/main/java/pdl_2018/groupeSMKS1/Url.java
+++ src/main/java/pdl_2018/groupeSMKS1/Url.java
@@ -3,10 +3,17 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.io.IOException;
+import com.google.common.net.InternetDomainName;
 
 import src.main.java.pdl_2018.groupeSMKS1.IUrl;
 
-public class Url implements IUrl{ 
+public class Url implements IUrl{
+	
+	private String url;
+	private char delimit;
+	private String nomCsv; 
+	private boolean extraWiki;
+	private boolean extraHtml;
 
 /**
  * 
@@ -14,36 +21,41 @@ public class Url implements IUrl{
  * @return true si l'URL est valide, false sinon
  * 
  */
-public boolean isValidURL(String url) {  
-URL u = null; 
-try { 
-    u = new URL(url); 
-} catch (MalformedURLException e) { 
-    return false; 
-} 
-try { 
-    URLConnection c = u.openConnection(); 
-    c.connect(); 
-} catch (IOException e) { 
-    return false; 
-} 
-return true; 
-}
-//test
+
 @Override
-public boolean verifURL(String URL) {
-	// TODO Auto-generated method stub
+public boolean verifURL(String url) {
+	URL u = null; 
+	try { 
+	    u = new URL(url); 
+	} catch (MalformedURLException e) { 
+	    return false; 
+	} 
+	try { 
+	    URLConnection c = u.openConnection(); 
+	    c.connect(); 
+	} catch (IOException e) { 
+	    return false; 
+	} 
+	return true; 
+}
+
+@Override
+public boolean isWikipediaURL(String url) {
+	URL u = null; 
+	try { 
+	    u = new URL(url); 
+	} catch (MalformedURLException e) { 
+	    return false; 
+	}
+	String host = u.getHost();
+	InternetDomainName name = InternetDomainName.from(host).topPrivateDomain();
+	System.out.println(host);
+    System.out.println(name);
 	return false;
 }
 
 @Override
-public boolean isWikipediaURL(String URL) {
-	// TODO Auto-generated method stub
-	return false;
-}
-
-@Override
-public Extracteur ConstructeurExtracteur(char delimit, String cheminCSV, String NomCSV, boolean extraHTML,
+public Extracteur ConstructeurExtracteur(char delimit, String cheminCSV, String nomCSV, boolean extraHTML,
 		boolean extraWiki) {
 	// TODO Auto-generated method stub
 	return null;

6027dd5450f1edc58b8766edb5c62757efc66872
Sophie <sophie.bouvry1@gmail.com>
2018-11-02 18:28:50 +0100

    Implémentation de l'interface URL

--- src/main/java/pdl_2018/groupeSMKS1/IUrl.java
+++ src/main/java/pdl_2018/groupeSMKS1/IUrl.java
@@ -1,8 +1,32 @@
 package src.main.java.pdl_2018.groupeSMKS1;
 
-import java.util.ArrayList;
-
 public interface IUrl {
+	
+	/**
+	 * 
+	 * @param URL est l'adresse URL d'une page web
+	 * @return True si l'URL est bien formé, false sinon
+	 */
+ public  boolean verifURL(String URL);
+ 
+ /**
+  * 
+  * @param URL est l'adresse URL d'une page web
+  * @return True si l'URL provient d'une page wikipedia, false sinon
+  */
+ public boolean isWikipediaURL(String URL);
+ 
 
-
+ /**
+  * 
+  * @param delimit le délimiteur choisit par l'utilisateur
+  * @param cheminCSV le chemin ou le fichier sera sauvegardé
+  * @param NomCSV Le nom du fichier CSV de sortie
+  * @param extraHTML Un boolean à true si l'extraction doit étre faite en HTML, false sinon
+  * @param extraWiki Un boolean à true si l'extraction doit etre faite avec le 	Wikicode, false sinon
+  * @return Retour un object extracteur
+  */
+ public Extracteur ConstructeurExtracteur( char delimit, String cheminCSV,String NomCSV, boolean extraHTML, boolean extraWiki);
 }
+
+

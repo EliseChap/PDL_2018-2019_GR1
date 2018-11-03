5f7c901e9d9fa9ccf823b6c96120b4750358cd05
Sophie <sophie.bouvry1@gmail.com>
2018-11-03 12:23:09 +0100

    Constructeur des classe wikitext et html ainsi que les assesseur et mutateurs

--- src/main/java/pdl_2018/groupeSMKS1/Extracteur.java
+++ src/main/java/pdl_2018/groupeSMKS1/Extracteur.java
@@ -2,4 +2,7 @@ package src.main.java.pdl_2018.groupeSMKS1;
 
 public abstract class Extracteur implements IExtracteur {
 
+	
+	
+	
 }
--- src/main/java/pdl_2018/groupeSMKS1/Html.java
+++ src/main/java/pdl_2018/groupeSMKS1/Html.java
@@ -1,20 +1,78 @@
 package src.main.java.pdl_2018.groupeSMKS1;
 
-public class Html extends Extracteur{
+public class Html extends Extracteur {
+
+	private char delimit;
+	private String cheminCSV;
+	private String nomCSV;
+	private boolean extraHTML;
+	private boolean extraWiki;
+
+	public Html(char delimit, String cheminCSV, String nomCSV, boolean extraHTML, boolean extraWiki) {
+		this.delimit = delimit;
+		this.cheminCSV = cheminCSV;
+		this.extraHTML = extraHTML;
+		this.extraWiki = extraWiki;
+	}
 
 	@Override
-	public void removeTableau() {}
-	
+	public void removeTableau() {
+	}
+
 	@Override
-	public String getNomTableau(){
+	public String getNomTableau() {
 		return "";
-		}
+	}
+
 	@Override
-	public void addTableau(){}
+	public void addTableau() {
+	}
+
 	@Override
-	public Tableau constructeurTableau(char delimit, String cheminCSV,String NomCSV, boolean extraHTML, boolean extraWiki)
-	{
+	public Tableau constructeurTableau(char delimit, String cheminCSV, String NomCSV, boolean extraHTML,
+			boolean extraWiki) {
 		return new Tableau();
 	}
-	
+
+	/**
+	 * 
+	 * @return le délimiteur choisit par l'utilisateur
+	 */
+	public char getDelimit() {
+		return this.delimit;
+	}
+
+	/**
+	 * 
+	 * @return le chemin de sauvegarde du fichier choisit par l'utilisateur
+	 */
+	public String getCheminCSV() {
+		return this.cheminCSV;
+	}
+
+	/**
+	 * 
+	 * @return le nom du fichier CSV choisit par l'utilisateur
+	 */
+	public String getNomCSV() {
+		return this.getNomCSV();
+	}
+
+	/**
+	 * 
+	 * @return un boolean qui indique si l'extraction doit étre faire en HTML (true)
+	 *         ou non (false)
+	 */
+	public boolean getExtraHTML() {
+		return this.extraHTML;
+	}
+
+	/**
+	 * 
+	 * @return Un boolean qui indique si l'extraction doit étre faire en
+	 *         wikicode(true) ou non (false)
+	 */
+	public boolean getExtraWiki() {
+		return this.extraWiki;
+	}
 }

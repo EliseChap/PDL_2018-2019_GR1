26b2ce8945dc9e813e41807b72255d2af6ca102b
Sophie <sophie.bouvry1@gmail.com>
2018-11-03 12:07:02 +0100

    Déclaration des méthodes override dans les classe HTML et Wikitext

--- src/main/java/pdl_2018/groupeSMKS1/Html.java
+++ src/main/java/pdl_2018/groupeSMKS1/Html.java
@@ -2,4 +2,19 @@ package src.main.java.pdl_2018.groupeSMKS1;
 
 public class Html extends Extracteur{
 
+	@Override
+	public void removeTableau() {}
+	
+	@Override
+	public String getNomTableau(){
+		return "";
+		}
+	@Override
+	public void addTableau(){}
+	@Override
+	public Tableau constructeurTableau(char delimit, String cheminCSV,String NomCSV, boolean extraHTML, boolean extraWiki)
+	{
+		return new Tableau();
+	}
+	
 }
--- src/main/java/pdl_2018/groupeSMKS1/Wikitext.java
+++ src/main/java/pdl_2018/groupeSMKS1/Wikitext.java
@@ -1,7 +1,19 @@
 package src.main.java.pdl_2018.groupeSMKS1;
 
-
-
 public class Wikitext extends Extracteur{
 
+	@Override
+	public void removeTableau() {}
+	
+	@Override
+	public String getNomTableau(){
+		return "";
+		}
+	@Override
+	public void addTableau(){}
+	@Override
+	public Tableau constructeurTableau(char delimit, String cheminCSV,String NomCSV, boolean extraHTML, boolean extraWiki)
+	{
+		return new Tableau();
+	}
 }

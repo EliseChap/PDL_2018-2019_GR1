27842e3042cb32d77853e2a15d7df021d355123e
Sophie <sophie.bouvry1@gmail.com>
2018-11-03 12:01:10 +0100

    Implémentation de l'interface extracteur

--- src/main/java/pdl_2018/groupeSMKS1/IExtracteur.java
+++ src/main/java/pdl_2018/groupeSMKS1/IExtracteur.java
@@ -2,4 +2,32 @@ package src.main.java.pdl_2018.groupeSMKS1;
 
 public interface IExtracteur {
 
+/**
+ * 
+ * @param delimit délimiteur choisit par l'utilisateur
+ * @param cheminCSV Chemin de sauvegarde du CSV final choisit par l'utilisateur
+ * @param NomCSV Nom duy fichier CSV choisit par l'utilisateur
+ * @param extraHTML Boolean à true si l'extraction doit étre fait en HTML, false sinon
+ * @param extraWiki Boolean à true si l'extraction doit étre fait AVEC LE WIKICODE, false sinon
+ * @return un objet tableau
+ */
+	public Tableau constructeurTableau(char delimit, String cheminCSV,String NomCSV, boolean extraHTML, boolean extraWiki);
+	
+	/**
+	 * 
+	 * @return Retourne le nom du tableau
+	 */
+	public String getNomTableau();
+	
+	/**
+	 * Supprime le tableau
+	 */
+	public void removeTableau();
+	
+	/**
+	 * Ajout un tableau
+	 */
+	public void addTableau();
+	
+	
 }

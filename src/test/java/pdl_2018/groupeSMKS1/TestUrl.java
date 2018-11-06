3fd045004964ed848ad1d3572518a35070ef086c
Margaux <Margaux@148.60.32.206>
2018-11-06 18:10:13 +0100

    TestURL tests unitaire a revoir

--- src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
@@ -1,11 +1,26 @@
 package src.test.java.pdl_2018.groupeSMKS1;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TestUrl {
 
+
+import junit.framework.TestCase;
+
+
+
+
+
+class TestUrl extends TestCase {
+	@Test
+	public void testURLstandard() {
+	//	Url u = new Url("https://fr.wikipedia.org/wiki/Saison_7_de_Game_of_Thrones",' ',"","",true,false);
+		
+		Assertions.assertTrue(true);
+		
+	}
 
 
 }

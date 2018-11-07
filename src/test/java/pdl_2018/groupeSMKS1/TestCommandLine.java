a1e81ee24363465a62b1172ae8dc77aebc5348a9
Kénan Lethimonier-Herout <kenan.lethimonier-herout@lkcn-informatique.com>
2018-11-07 23:53:53 +0100

    TestCommandLine : Début mise en place des tests

--- src/test/java/pdl_2018/groupeSMKS1/TestCommandLine.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestCommandLine.java
@@ -1,10 +1,30 @@
-package src.test.java.pdl_2018.groupeSMKS1;
+package pdl_2018.groupeSMKS1;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 class TestCommandLine {
 
+    /**
+     * Tests pour la fonction verifDelimiteur
+     */
+    @Test
+    public void testVerifDelimiteur() {
 
+        //Tests de la commande -import uniquement
+        CommandLine myCommand = new CommandLine("-import[;]");
+        Assertions.assertTrue(myCommand.verifDelimiteur(myCommand.ligneDeCommande), "true");
+        Assertions.assertEquals(myCommand.delimit, ';');
+
+        CommandLine myCommand2 = new CommandLine("-import[]");
+        Assertions.assertTrue(myCommand2.verifDelimiteur(myCommand.ligneDeCommande), "false");
+        Assertions.assertEquals(myCommand.delimit, null);
+
+        CommandLine myCommand3 = new CommandLine("import[;]");
+        Assertions.assertTrue(myCommand3.verifDelimiteur(myCommand.ligneDeCommande), "false");
+        Assertions.assertEquals(myCommand.delimit, null);
+
+        //ToDO Tests avec une ligne de commande entière
+    }
 }

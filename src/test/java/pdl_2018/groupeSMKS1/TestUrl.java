184b82c5ae93f4ee34e0d57c366a8e26579fe3ce
Sophie <sophie.bouvry1@gmail.com>
2018-11-07 16:51:16 +0100

    Test du bug de margaux, modification d'une fonction

--- src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
@@ -5,9 +5,10 @@ package src.test.java.pdl_2018.groupeSMKS1;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
+import com.sun.xml.bind.Util;
 
 import junit.framework.TestCase;
+import src.main.java.pdl_2018.groupeSMKS1.Url;
 
 
 
@@ -16,11 +17,17 @@ import junit.framework.TestCase;
 class TestUrl extends TestCase {
 	@Test
 	public void testURLstandard() {
-	//	Url u = new Url("https://fr.wikipedia.org/wiki/Saison_7_de_Game_of_Thrones",' ',"","",true,false);
+		Url u = new Url("https://fr.wikipedia.org/wiki/Saison_7_de_Game_of_Thrones",' ',"","",true,false);
 		
-		Assertions.assertTrue(true);
+		Assertions.assertTrue(u.verifURL());
 		
 	}
-
+	@Test
+	public void testURLNonValide() {
+		Url u = new Url("https://fr.wikipedia.",' ',"","",true,false);
+		
+	//	Assertions.assertFalse(u.verifURL());
+		
+	}
 
 }

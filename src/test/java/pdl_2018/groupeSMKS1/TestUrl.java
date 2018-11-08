bf8a771252cc60b694f9f8ceb8c6cbb2c1f79abe
Margaux <Margaux@DESKTOP-V88FTER>
2018-11-08 11:10:09 +0100

    liste de tests

--- src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
@@ -21,20 +21,37 @@ class TestUrl extends TestCase {
 	public void testURLstandard() {
 		Assertions.assertTrue(u.verifURL());
 		Assertions.assertTrue(u.isWikipediaURL());
-	}
+	}// Faire dautre set et asserions dans le focntion
 
 	@Test
 	public void testURLNonValide() {
 		u.setUrl("https://fr.wikipedia.");
 		Assertions.assertFalse(u.verifURL());
 
-	}
+	} // Faire dautre set et asserions dans le focntion 
 
 	@Test
-	public void testisWikiURLFalse() {
+	public void testIsWikiURLFalse() {
 		u.setUrl("https://github.com/SulliDai/PDL_2018-2019_GR1");
 		Assertions.assertFalse(u.isWikipediaURL());
 
-	}
+	}// Faire dautre set et asserions dans le focntion
+	
+	@Test 
+	public void testIsPNG() {}
+	
+	@Test 
+	public void testIsJPEG() {}
+	
+	@Test 
+	public void testIsDiscussion() {}
+	
+	@Test 
+	public void testIsContributions() {}
+	
+	@Test 
+	public void testIsRealURL() {}
+	
+	
 	
 }

fb100686271dcb23bbf7fff62248a5c5a945b93f
Kénan Lethimonier-Herout <kenan.lethimonier-herout@lkcn-informatique.com>
2018-11-08 16:59:31 +0100

    TestCommandLine : suppression des arguments des sous-fonctions

--- src/test/java/pdl_2018/groupeSMKS1/TestCommandLine.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestCommandLine.java
@@ -14,15 +14,15 @@ class TestCommandLine {
 
         //Tests de la commande -import uniquement
         CommandLine myCommand = new CommandLine("-import[;]");
-        Assertions.assertTrue(myCommand.verifDelimiteur(myCommand.ligneDeCommande), "true");
+        Assertions.assertTrue(myCommand.verifDelimiteur(), "true");
         Assertions.assertEquals(myCommand.delimit, ';');
 
         CommandLine myCommand2 = new CommandLine("-import[]");
-        Assertions.assertTrue(myCommand2.verifDelimiteur(myCommand.ligneDeCommande), "false");
+        Assertions.assertTrue(myCommand2.verifDelimiteur(), "false");
         Assertions.assertEquals(myCommand.delimit, null);
 
         CommandLine myCommand3 = new CommandLine("import[;]");
-        Assertions.assertTrue(myCommand3.verifDelimiteur(myCommand.ligneDeCommande), "false");
+        Assertions.assertTrue(myCommand3.verifDelimiteur(), "false");
         Assertions.assertEquals(myCommand.delimit, null);
 
         //ToDO Tests avec une ligne de commande entière
@@ -34,32 +34,32 @@ class TestCommandLine {
     @Test
     public void testverifHtmlOrWikicodeChoice(){
         CommandLine myCommand = new CommandLine("-html");
-        Assertions.assertTrue(myCommand.verifHtmlOrWikicodeChoice(myCommand.ligneDeCommande));
+        Assertions.assertTrue(myCommand.verifHtmlOrWikicodeChoice());
         Assertions.assertTrue(myCommand.extraHTML);
         Assertions.assertFalse(myCommand.extraWiki);
 
         CommandLine myCommand2 = new CommandLine("-wiki");
-        Assertions.assertTrue(myCommand2.verifHtmlOrWikicodeChoice(myCommand.ligneDeCommande));
+        Assertions.assertTrue(myCommand2.verifHtmlOrWikicodeChoice());
         Assertions.assertTrue(myCommand2.extraWiki);
         Assertions.assertFalse(myCommand2.extraHTML);
 
         CommandLine myCommand3 = new CommandLine("-wiki -html");
-        Assertions.assertTrue(myCommand3.verifHtmlOrWikicodeChoice(myCommand.ligneDeCommande));
+        Assertions.assertTrue(myCommand3.verifHtmlOrWikicodeChoice());
         Assertions.assertTrue(myCommand3.extraWiki);
         Assertions.assertTrue(myCommand3.extraHTML);
 
         CommandLine myCommand4 = new CommandLine("");
-        Assertions.assertFalse(myCommand4.verifHtmlOrWikicodeChoice(myCommand.ligneDeCommande));
+        Assertions.assertFalse(myCommand4.verifHtmlOrWikicodeChoice());
         Assertions.assertFalse(myCommand4.extraWiki);
         Assertions.assertFalse(myCommand4.extraHTML);
 
         CommandLine myCommand5 = new CommandLine("html");
-        Assertions.assertFalse(myCommand5.verifHtmlOrWikicodeChoice(myCommand.ligneDeCommande));
+        Assertions.assertFalse(myCommand5.verifHtmlOrWikicodeChoice());
         Assertions.assertFalse(myCommand5.extraHTML);
         Assertions.assertFalse(myCommand5.extraWiki);
 
         CommandLine myCommand6 = new CommandLine("wiki");
-        Assertions.assertFalse(myCommand6.verifHtmlOrWikicodeChoice(myCommand.ligneDeCommande));
+        Assertions.assertFalse(myCommand6.verifHtmlOrWikicodeChoice());
         Assertions.assertFalse(myCommand6.extraWiki);
         Assertions.assertFalse(myCommand6.extraHTML);
     }

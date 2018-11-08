6c89e9730b6140da2cdbb48be015180e93d04b0f
Margaux <Margaux@DESKTOP-V88FTER>
2018-11-08 11:06:39 +0100

    Création private Url u

--- src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestUrl.java
@@ -1,5 +1,6 @@
 package src.test.java.pdl_2018.groupeSMKS1;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,24 +10,29 @@ import junit.framework.TestCase;
 import src.main.java.pdl_2018.groupeSMKS1.Url;
 
 class TestUrl extends TestCase {
+	
+	private Url u;
+	
+	@Before 
+	public void initialise() {
+		u = new Url("https://fr.wikipedia.org/wiki/Saison_7_de_Game_of_Thrones", ' ', "", "", true, false);
+	}
 	@Test
 	public void testURLstandard() {
-		Url u = new Url("https://fr.wikipedia.org/wiki/Saison_7_de_Game_of_Thrones", ' ', "", "", true, false);
-
 		Assertions.assertTrue(u.verifURL());
 		Assertions.assertTrue(u.isWikipediaURL());
 	}
 
 	@Test
 	public void testURLNonValide() {
-		Url u = new Url("https://fr.wikipedia.", ' ', "", "", true, false);
+		u.setUrl("https://fr.wikipedia.");
 		Assertions.assertFalse(u.verifURL());
 
 	}
 
 	@Test
 	public void testisWikiURLFalse() {
-		Url u = new Url("https://github.com/SulliDai/PDL_2018-2019_GR1", ' ', "", "", true, false);
+		u.setUrl("https://github.com/SulliDai/PDL_2018-2019_GR1");
 		Assertions.assertFalse(u.isWikipediaURL());
 
 	}

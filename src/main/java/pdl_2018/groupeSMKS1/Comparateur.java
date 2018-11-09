d2edc224c8a1990c215abb5892f33402916f8163
sullivan <sullivan.dairou@etudiant.univ-rennes1.fr>
2018-11-09 19:12:13 +0100

    Modification méthode comparaison

--- src/main/java/pdl_2018/groupeSMKS1/Comparateur.java
+++ src/main/java/pdl_2018/groupeSMKS1/Comparateur.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 import org.apache.commons.collections4.ListUtils;
 
 public class Comparateur implements IComparateur{
-	ArrayList<String[]> wiki;
-	ArrayList<String[]> html ;
+	private ArrayList<String[]> wiki;
+	private ArrayList<String[]> html ;
 	
 	public Comparateur(ArrayList<String[]> wiki,ArrayList<String[]> html ){
 		this.wiki=wiki;
@@ -18,16 +18,33 @@ public class Comparateur implements IComparateur{
 		int nbListWiki = wiki.size();
 		int nbListHtml = html.size();
 		boolean identique = true;
+		String[] tabWiki;
+		String[] tabHtml;
+		
 		int i = 0;
-		while(i<nbListWiki && i<nbListHtml) {
-			String[] tabWiki = wiki.get(i);
-			String[] tabHtml = html.get(i);
+		while(i<nbListWiki || i<nbListHtml) {
+			if(i>=nbListWiki) {
+				tabHtml = html.get(i);
+				identique=false;
+				System.out.println("ligne "+i+" differente html"+" : "+Arrays.asList(tabHtml).toString());
+				System.out.println("ligne "+i+" differente wiki"+" : ");
+			}
+			if(i>=nbListHtml) {
+				tabWiki = wiki.get(i);
+				identique=false;
+				System.out.println("ligne "+i+" differente wiki"+" : "+Arrays.asList(tabWiki).toString());
+				System.out.println("ligne "+i+" differente html"+" : ");
+			}
+			
+			if(i<nbListWiki && i<nbListHtml) {
+				tabHtml = html.get(i);
+				tabWiki = wiki.get(i);
 			if(!Arrays.equals(tabWiki, tabHtml)) {
 				identique=false;
 				System.out.println("diffrérence entre les deux fichiers :");
-				System.out.println("wiki"+" : "+Arrays.asList(tabWiki).toString());
-				System.out.println("html"+" : "+Arrays.asList(tabHtml).toString());
-			}
+				System.out.println("ligne "+i+" differente wiki"+" : "+Arrays.asList(tabWiki).toString());
+				System.out.println("ligne "+i+" differente html"+" : "+Arrays.asList(tabHtml).toString());
+			}}
 			i++;
 		}
 		return identique;
@@ -45,10 +62,13 @@ public class Comparateur implements IComparateur{
 		
 		
 		ArrayList<String[]> list3 = new ArrayList<String[]>();
-		String[] arr4 = { "a;", "b", "c" };
+		String[] arr4 = { "a2;", "b", "c", "e" };
+		String[] arr5 = { "a2;" };
 		String[] arr3 = { "1,0:-", "2", "3", "4" };
 		list3.add(arr4);
+		list3.add(arr5);
 		list3.add(arr3);
+
 		
 		Comparateur comparateur = new Comparateur(list, list3);
 		comparateur.comparaison();

27d56d1b345c2426e4b18a2b1ee216bb57226aef
Sophie <sophie.bouvry1@gmail.com>
2018-11-09 17:10:42 +0100

    ajout fonction parcourir node

--- src/main/java/pdl_2018/groupeSMKS1/Wikitext.java
+++ src/main/java/pdl_2018/groupeSMKS1/Wikitext.java
@@ -2,6 +2,7 @@ package src.main.java.pdl_2018.groupeSMKS1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.sweble.wikitext.engine.ExpansionCallback;
@@ -11,6 +12,7 @@ import org.sweble.wikitext.engine.WtEngineImpl;
 import org.sweble.wikitext.engine.config.WikiConfig;
 import org.sweble.wikitext.engine.nodes.EngProcessedPage;
 import org.sweble.wikitext.engine.utils.DefaultConfigEnWp;
+import org.sweble.wikitext.parser.nodes.WtNode;
 import org.wikipedia.Wiki;
 
 public class Wikitext extends Extracteur {
@@ -59,11 +61,27 @@ public class Wikitext extends Extracteur {
 		PageId pageId = new PageId(pageTitle, -1);
 		ExpansionCallback callback = null;
 		EngProcessedPage parse = engine.parse(pageId, contenu, callback);
+		parcourirNode(parse);
 			} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
+		
+	private static void parcourirNode(WtNode fils) {
+		for (Iterator<WtNode> l = fils.iterator(); l.hasNext();) {
+			fils = l.next();
+			if (fils.getNodeName().toString().equals("WtTable")) {
+				System.out.println(fils.getNodeName().toString());
+				String wikitable = fils.toString();
+				//System.out.println(wikitable.indexOf("wikitable"));
+				if(wikitable.indexOf("wikitable")!=-1){
+				//Insersion dans une liste de tableau
+				System.out.println("TROUVERTOUVER");}}
+			parcourirNode(fils);
+		}
+}
 	
 	
 	@Override

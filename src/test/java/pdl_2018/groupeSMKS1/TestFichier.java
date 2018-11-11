a8948fbe7b7970fbe8ee710b36bd0535b77ca143
Kénan Lethimonier-Herout <kenan.lethimonier-herout@lkcn-informatique.com>
2018-11-12 00:33:17 +0100

    TestFichier : Squelette des tests

--- src/test/java/pdl_2018/groupeSMKS1/TestFichier.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestFichier.java
@@ -3,9 +3,36 @@ package src.test.java.pdl_2018.groupeSMKS1;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
+import src.main.java.pdl_2018.groupeSMKS1.Fichier;
 
 class TestFichier {
 
+    private static Fichier f;
+
+    @BeforeAll
+    public static void initialise() {
+
+        f = new Fichier("c:/mesURLsWikipedia.txt", ' ', "", "", true, false);
+
+    }
+
+    /**
+     * Tests pour la méthode traitementFichierEntree
+     */
+    @Test
+    public void testTraitementFichierEntree() {
+
+    }
+
+    /**
+     * Tests pour la méthode decoupageAndGenerationURLs
+     */
+    @Test
+    public void testVerifDelimiteur() {
+
+    }
 
 
 }

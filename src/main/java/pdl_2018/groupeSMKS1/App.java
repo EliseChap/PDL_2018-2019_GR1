a43f52d4e40ddf4288464c5152f96a18177fc4c4
sullivan <sullivan.dairou@etudiant.univ-rennes1.fr>
2018-11-21 01:06:27 +0100

    premiers test html vers csv

--- src/main/java/pdl_2018/groupeSMKS1/App.java
+++ src/main/java/pdl_2018/groupeSMKS1/App.java
@@ -7,9 +7,9 @@ public class App {
 		
 		char delimit = ';';
 		String url ="https://fr.wikipedia.org/wiki/Stranger_Things";
-		String cheminCSV = "";
+		String cheminCSV = "C:/Users/sullivand/Music/Desktop/";
 		boolean extraHTML = true;
-		String nomCSV = "Html_Csv";
+		String nomCSV = "Html_Csv.csv";
 		boolean extraWiki = false;
 		Html html = new Html(url, delimit, cheminCSV, nomCSV, extraHTML, extraWiki);
 		html.recuperationPage();

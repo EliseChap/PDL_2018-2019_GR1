19f0a7f57fc58f0ac9c3c5fec21ff2536b65787c
sullivan <sullivan.dairou@etudiant.univ-rennes1.fr>
2018-11-25 15:43:10 +0100

    corriger erreur

--- src/test/java/pdl_2018/groupeSMKS1/TestTableau.java
+++ src/test/java/pdl_2018/groupeSMKS1/TestTableau.java
@@ -19,10 +19,10 @@ class TestTableau {
 		
 		String tab[][] = { { "0", "2a", "4h", "6", "fds8" }, { "sQ1", "3", "GRE5", "7", "D9" } };
 
-		Tableau csv = new Tableau(';', "", "WikiMatrix", tab,"tableau");
+		Tableau csv = new Tableau(';', "", "WikiMatrix.csv", tab,"tableau");
 		Assertions.assertEquals(csv.getDelimit(), ';');
 		Assertions.assertEquals(csv.getCheminCsv(), "");
-		Assertions.assertEquals(csv.getNomCsv(), "WikiMatrix");
+		Assertions.assertEquals(csv.getNomCsv(), "WikiMatrix.csv");
 		System.out.println(csv.getTableauToString());
 		Assertions.assertEquals(csv.getTableauToString(),"[0, 2a, 4h, 6, fds8][sQ1, 3, GRE5, 7, D9]");
 

c299dbc091aa385796252e482e9d564065867617
Kénan Lethimonier-Herout <kenan.lethimonier-herout@lkcn-informatique.com>
2018-12-20 22:52:00 +0100

    Traduction en anglais : App.java

--- src/main/java/pdl_2018/groupeSMKS1/App.java
+++ src/main/java/pdl_2018/groupeSMKS1/App.java
@@ -18,15 +18,15 @@ public class App {
 	}
 
 	public static void afficherMessageBienvenue(){
-		System.out.println("Bienvenue sur Wikimatrix, l'extracteur de tableaux Wikipédia made in Rennes !");
-		System.out.println("Pour commencer à extraire de la donnée, saisissez une commande ci-dessous puis tapez \"entrée\".");
-		System.out.println("Pour obtenir la liste des commandes obligatoires ou optionnelles disponibles, tapez directement \"entrée\".");
+		System.out.println("Welcome to Wikimatrix, the Wikipedia tables extractor made in Rennes!");
+		System.out.println("To start extracting data, write a command below and type \"Enter\".");
+		System.out.println("For a list of the required or optional commands available, type directly \"Enter\".");
 	}
 
 	public static CommandLine demanderCommande(){
 		
 		Scanner saisieClavier = new Scanner(System.in);
-		System.out.println("VOTRE COMMANDE >>> ");
+		System.out.println("YOUR COMMAND >>> ");
 		String ldc = saisieClavier.nextLine();
 
 		if(ldc==null || ldc==""){
@@ -34,17 +34,17 @@ public class App {
 		}else{
 			return new CommandLine(ldc);
 		}
-return null;
+		return null;
 	}
 
 	public static void afficherCommandesDisponibles(){
-		System.out.println("(obligatoire) -html et/ou -wikicode : permet d'indiquer votre préférence pour une extraction à base d'HTML ou de Wikicode");
-		System.out.println("-import[...] : permet de préciser un fichier d'entrée au format .txt, pour extraire à partir de plusieurs URLs simultanément");
-		System.out.println("-save[...] : permet de préciser un répertoire de sortie pour les fichiers CSV générés.");
-		System.out.println("-name[...] : permet de préciser un nom pour les fichiers CSV générés.");
-		System.out.println("-delimit[...] : permet de spécifier un délimiteur \n");
+		System.out.println("(mandatory) -html and/or -wikicode : allows you to specify your preference for HTML-based or Wikicode-based extraction");
+		System.out.println("-import[...] : allows you to specify an input file in .txt format, to extract from multiple URLs simultaneously");
+		System.out.println("-save[...] : allows you to specify an output directory for generated CSV files");
+		System.out.println("-name[...] : allows you to specify a name for generated CSV files");
+		System.out.println("-delimit[...] : allows you to specify a delimiter \n");
 
-		demanderCommande(); //Après affichage des options disponibles, on relance la demande de saisie d'une commande.
+		demanderCommande(); //After displaying the available options, restart the request to enter a command.
 	}
 }
 
